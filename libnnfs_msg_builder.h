bdc6cc9d590443b1bf0727fbe074ff375f510b08
ammVadimCh <92300162+ammVadimCh@users.noreply.github.com>
2022-07-10 17:26:54 +0300

    Update libnnfs_msg_builder.h

--- libnnfs_msg_builder.h
+++ libnnfs_msg_builder.h
@@ -8,6 +8,7 @@
 #define NULL_OP_CODE 256u 
 #define OP_CODE_CONNECT_CALL 257u
 #define OP_CODE_CLOSE_CONNECTION 258u
+#define OP_CODE_PING 259u
 
 //not implemented op_codes:
 #define OP_CODE_LIST_DIRECTORY 259u
@@ -15,9 +16,9 @@
 
 
 //op_code in TYPE_REPLY represents status
-#define STATUS_SUCCESSFUL 0u
+#define STATUS_SUCCESS 0u
 #define STATUS_FAIL 1u
-
+#define STATUS_PONG 2u
 
 //defines a basic template call
 void inner_build_template_call(struct MSG *message, uint32_t ID){
@@ -39,44 +40,54 @@ void inner_build_template_reply(struct MSG *message, uint32_t ID){
 
 //builds connect call, ID is provided from outside
 //right now the payload is empty
-struct MSG build_connect_call(uint32_t ID){
-    struct MSG retval;
-    inner_build_template_call(&retval, ID);
-    retval.op_code = OP_CODE_CONNECT_CALL;
-    return retval;
+void build_connect_call(struct MSG *retval, uint32_t ID){
+    destroymsg(*retval);
+    inner_build_template_call(retval, ID);
+    retval->op_code = OP_CODE_CONNECT_CALL;
 }
 
 //builds reply on successful connection
 //op_code is used as a STATUS in the reply message
-struct MSG build_success_reply(uint32_t ID){
-    struct MSG retval;
-    inner_build_template_reply(&retval, ID);
-    retval.op_code = STATUS_SUCCESSFUL;
-    return retval;
+void build_success_reply(struct MSG *retval, uint32_t ID){
+    destroymsg(*retval);
+    inner_build_template_reply(retval, ID);
+    retval->op_code = STATUS_SUCCESS;
 }
 
 
 //builds reply on FAILED connection
 //op_code is used as a STATUS in the reply message
 //the reason why connection failed will be stated in status and payload contains more information
-struct MSG build_fail_reply(uint32_t ID, uint32_t reason, uint32_t length, unsigned char *payload){
-    struct MSG retval;
-    inner_build_template_reply(&retval, ID);
-    retval.op_code = reason;
-    retval.payload_len = length;
+void build_fail_reply(struct MSG *retval, uint32_t ID, uint32_t reason, uint32_t length, unsigned char *payload){
+    destroymsg(*retval);
+    inner_build_template_reply(retval, ID);
+    retval->op_code = reason;
+    retval->payload_len = length;
     if(length == 0)
-        retval.payload = NULL;
+        retval->payload = NULL;
     else{
-        retval.payload = (unsigned char*) calloc(length, 1);
-        strncpy((char*)retval.payload, (char *)payload, length);
+        retval->payload = (unsigned char*) calloc(length, 1);
+        strncpy((char*)retval->payload, (char *)payload, length);
     }
-    return retval;
 }
 
 //builds call to close connection
-struct MSG build_quit_call(uint32_t ID){
-    struct MSG retval;
-    inner_build_template_call(&retval, ID);
-    retval.op_code = OP_CODE_CLOSE_CONNECTION;
-    return retval;
+void build_quit_call(struct MSG *retval, uint32_t ID){
+    destroymsg(*retval);
+    inner_build_template_call(retval, ID);
+    retval->op_code = OP_CODE_CLOSE_CONNECTION;
+}
+
+//ID for ping=pong is always 0, it is reserved
+//is used to test if the client is still present
+void build_ping_call(struct MSG *retval){
+    destroymsg(*retval);
+    inner_build_template_call(retval, 0);
+    retval->op_code = OP_CODE_PING;
+}
+//is used to repsond to a ping call
+void build_pong_call(struct MSG *retval){
+    destroymsg(*retval);
+    inner_build_template_reply(retval, 0);
+    retval->op_code = STATUS_PONG;
 }

3b7e6a4f3c37f8ac17501f595dc36a37d983a10f
ammVadimCh <92300162+ammVadimCh@users.noreply.github.com>
2022-07-10 17:26:15 +0300

    Update libnnfs_proto.h

--- libnnfs_proto.h
+++ libnnfs_proto.h
@@ -8,6 +8,7 @@
 #define BITS_IN_BYTE 8u
 #define TYPE_CALL 0u
 #define TYPE_REPLY 1u
+#define NNFS_MAX_LENGTH 512u
 
 
 //ID used to send a reply to a specific call message by a server
@@ -39,63 +40,77 @@ uint32_t convert_XDR_to_uint32(unsigned char* read_from){
 }
 
 //to make reading of the code easier
+//should be initialised on first call
 struct encoded_message{
     unsigned char *mes;
     uint32_t length;
 };
 
-//converts from C struct message to the XDR format that can be send by a socket
-struct encoded_message encode(struct MSG toencode){
-    struct encoded_message encmes;
-    encmes.mes = (unsigned char*) calloc(1, MSG_HEADER_SIZE + toencode.payload_len);
-    encmes.length = MSG_HEADER_SIZE + toencode.payload_len;
-
-    //probably temporary
-    convert_uint32_to_XDR(toencode.ID, encmes.mes);
-    convert_uint32_to_XDR(toencode.type, encmes.mes + BYTES_IN_UINT32);
-    convert_uint32_to_XDR(toencode.op_code, encmes.mes + 2* BYTES_IN_UINT32);
-    convert_uint32_to_XDR(toencode.payload_len, encmes.mes + 3* BYTES_IN_UINT32);
-
-    unsigned char* src_ptr = toencode.payload;
-    unsigned char* dest_ptr = encmes.mes + 4*BYTES_IN_UINT32;
-    for(int i = 0; i < toencode.payload_len; i++, src_ptr++,dest_ptr++){
-        *dest_ptr = *src_ptr;
-    }
-    return encmes;
-}
-
-//TODO: debug
-struct MSG decode(struct encoded_message encmes){
-    struct MSG retval;
-    retval.ID = convert_XDR_to_uint32(encmes.mes);
-    retval.type = convert_XDR_to_uint32(encmes.mes + BYTES_IN_UINT32);
-    retval.op_code = convert_XDR_to_uint32(encmes.mes + 2* BYTES_IN_UINT32);
-    retval.payload_len = convert_XDR_to_uint32(encmes.mes + 3*BYTES_IN_UINT32);
-
-    if(retval.payload_len == 0) 
-        retval.payload = NULL;
-    else{
-        unsigned char* src_ptr = encmes.mes + 4*BYTES_IN_UINT32;
-        retval.payload = (unsigned char*) calloc(retval.payload_len, 1);
-        unsigned char *dest_ptr = retval.payload;
-        for(int i = 0; i < retval.payload_len; i++, dest_ptr++, src_ptr++){
-            *dest_ptr = *src_ptr;
-        }
-    }
-    return retval;
-}
-
 
 //uniform way to destroy structs introduced in this lib
-void destroy(struct MSG message){
+void destroymsg(struct MSG message){
     if(message.payload_len != 0){
         free(message.payload);
     }
+    message.payload_len = 0u;
     message.payload = NULL;
 }
 
-void destroy(struct encoded_message encmes){
+void destroyem(struct encoded_message encmes){
     if(encmes.mes != NULL)
         free(encmes.mes);
     encmes.mes = NULL;
+}
+
+//inits structures
+void init_encoded_message(struct encoded_message *encmes){
+    encmes->length = 0u;
+    encmes->mes = (unsigned char*) calloc(1, NNFS_MAX_LENGTH);
+}
+
+void init_MSG(struct MSG *message){
+    message->payload = NULL;
+    message->payload_len = 0u;
+}
+
+//converts from C struct message to the XDR format that can be send by a socket
+void encode(struct MSG toencode, struct encoded_message *encmes){
+    encmes->length = MSG_HEADER_SIZE + toencode.payload_len;
+    if(encmes->length > NNFS_MAX_LENGTH)
+        exit(1);
+
+    //probably temporary
+    convert_uint32_to_XDR(toencode.ID, encmes->mes);
+    convert_uint32_to_XDR(toencode.type, encmes->mes + BYTES_IN_UINT32);
+    convert_uint32_to_XDR(toencode.op_code, encmes->mes + 2* BYTES_IN_UINT32);
+    convert_uint32_to_XDR(toencode.payload_len, encmes->mes + 3* BYTES_IN_UINT32);
+
+    unsigned char* src_ptr = toencode.payload;
+    unsigned char* dest_ptr = encmes->mes + 4*BYTES_IN_UINT32;
+    for(int i = 0; i < toencode.payload_len; i++, src_ptr++,dest_ptr++){
+        *dest_ptr = *src_ptr;
+    }
+}
+
+//decodes encoded message
+void decode(struct encoded_message encmes, struct MSG *retval){
+    retval->ID = convert_XDR_to_uint32(encmes.mes);
+    retval->type = convert_XDR_to_uint32(encmes.mes + BYTES_IN_UINT32);
+    retval->op_code = convert_XDR_to_uint32(encmes.mes + 2* BYTES_IN_UINT32);
+    retval->payload_len = convert_XDR_to_uint32(encmes.mes + 3*BYTES_IN_UINT32);
+    if(retval->payload_len + MSG_HEADER_SIZE > NNFS_MAX_LENGTH)
+        exit(1);
+    
+    if(retval->payload != NULL)
+        destroymsg(*retval);
+    if(retval->payload_len == 0) 
+        retval->payload = NULL;
+    else{
+        unsigned char* src_ptr = encmes.mes + 4*BYTES_IN_UINT32;
+        retval->payload = (unsigned char*) calloc(retval->payload_len, 1);
+        unsigned char *dest_ptr = retval->payload;
+        for(int i = 0; i < retval->payload_len; i++, dest_ptr++, src_ptr++){
+            *dest_ptr = *src_ptr;
+        }
+    }
 }

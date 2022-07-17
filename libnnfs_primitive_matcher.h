0dcbf8a5841fe49c989111c93ec531a828361be8
ammVadimCh <92300162+ammVadimCh@users.noreply.github.com>
2022-07-17 21:04:26 +0300

    Create libnnfs_primitive_matcher.h

---
+++ libnnfs_primitive_matcher.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//matching ipv4 address and port from a command
+void match_IPaddr(const char* ,char **);
+void match_PORTnumber(const char*, char **);
+
+//gets a number from a string
+int match_client_number(const char*);
+
+
+//matchers for distinguishing user input commands
+//gets the type of command
+int type_of_command(const char*);
